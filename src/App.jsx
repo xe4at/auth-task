6e163c2f40804679cfe1fed9bf2d0da7ccc4094a
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2025-02-25 20:53:11 +0330

    fixed some problem

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,18 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "../src/pages/Login";
 import Me from "../src/pages/Me";
 import PageNotFound from "../src/pages/PageNotFound";
+import PrivateRoute from "../src/router/PrivateRoute";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/me" element={<Me />} />
+        <Route path="/me" element={
+          <PrivateRoute>
+            <Me />
+          </PrivateRoute>
+        } />
         <Route path="/" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
--- src/router/PrivateRoute.jsx
+++ src/router/PrivateRoute.jsx
@@ -1,28 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "../src/pages/Login";
-import Me from "../src/pages/Me";
-import PageNotFound from "../src/pages/PageNotFound";
-import PrivateRoute from "../router/PrivateRoute";
+import { Navigate } from "react-router-dom";
 
-const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route
-          path="/me"
-          element={
-            <PrivateRoute>
-              <Me />
-            </PrivateRoute>
-          }
-        />
-        <Route path="/" element={<Login />} />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-    </Router>
-  );
+const PrivateRoute = ({ children }) => {
+  const isAuthenticated = !!localStorage.getItem("accessToken");
+
+  return isAuthenticated ? children : <Navigate to="/login" />;
 };
 
-export default App;
+export default PrivateRoute;
